184d3b71a7e5614cb97b5936ff10a321d0e5e0f4
yangchao5@100tal.com <yangchao5@100tal.com>
2017-04-18 14:58:57 +0800

    add

---
+++ 前端培训/app.js
@@ -0,0 +1,138 @@
+var app = {
+    win: $(window),
+    cookie: {
+        set: function (name, value, options) {
+            if (!options) {
+                options = options || {
+                };
+            }
+            var hour = options.hour ? options.hour : 0;
+            var path = options.path ? options.path + ';' : '/';
+            if (hour) {
+                var today = new Date();
+                var expire = new Date();
+                expire.setTime(today.getTime() + 3600000 * hour);
+            }
+            window.document.cookie = name + '=' + encodeURI(value) + '; ' + (hour ? ('expires=' + expire.toGMTString() + '; ')  : '') + ('path=' + path);
+            return true;
+        },
+        get: function (name) {
+            var reg = new RegExp('(?:^|;+|\\s+)' + name + '=([^;]*)');
+            var m = window.document.cookie.match(reg);
+            return (!m ? '' : decodeURI(m[1]));
+        },
+        remove: function (name, path) {
+            var path = path ? path + ';' : '/';
+            window.document.cookie = name + '=; expires=Mon, 1 Jun 2014 01:00:00 GMT; ' + (path ? ('path=' + path + '; ')  : 'path=/; ');
+        }
+    },
+    localStorage: {
+        set: function (key, value) {
+            if (window.localStorage) {
+                localStorage.setItem(key, value);
+            } else {
+                app.cookie.set(key, value);
+            }
+        },
+        get: function (key) {
+            if (window.localStorage) {
+                return localStorage.getItem(key);
+            } else {
+                return app.cookie.set(key);
+            }
+        },
+        remove: function (key) {
+            if (window.localStorage) {
+                localStorage.removeItem(key);
+            } else {
+                app.cookie.remove(key);
+            }
+        },
+        clear: function () {
+            if (window.localStorage) {
+                localStorage.clear();
+            } else {
+            }
+        }
+    },
+    isMobile: function (str) {
+        var regu = /^[1][0-9]{10}$/;
+        var re = new RegExp(regu);
+        if (re.test(str)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+(function ($) {
+    function detect(ua) {
+        var os = this.os = {
+        },
+        browser = this.browser = {
+        },
+        webkit = ua.match(/Web[kK]it[\/]{0,1}([\d.]+)/),
+        android = ua.match(/(Android);?[\s\/]+([\d.]+)?/),
+        osx = !!ua.match(/\(Macintosh\; Intel /),
+        ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
+        ipod = ua.match(/(iPod)(.*OS\s([\d_]+))?/),
+        iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
+        webos = ua.match(/(webOS|hpwOS)[\s\/]([\d.]+)/),
+        wp = ua.match(/Windows Phone ([\d.]+)/),
+        touchpad = webos && ua.match(/TouchPad/),
+        kindle = ua.match(/Kindle\/([\d.]+)/),
+        silk = ua.match(/Silk\/([\d._]+)/),
+        blackberry = ua.match(/(BlackBerry).*Version\/([\d.]+)/),
+        bb10 = ua.match(/(BB10).*Version\/([\d.]+)/),
+        rimtabletos = ua.match(/(RIM\sTablet\sOS)\s([\d.]+)/),
+        playbook = ua.match(/PlayBook/),
+        chrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/),
+        firefox = ua.match(/Firefox\/([\d.]+)/),
+        ie = ua.match(/MSIE\s([\d.]+)/) || ua.match(/Trident\/[\d](?=[^\?]+).*rv:([0-9.].)/),
+        webview = !chrome && ua.match(/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/),
+        safari = webview || ua.match(/Version\/([\d.]+)([^S](Safari)|[^M]*(Mobile)[^S]*(Safari))/),
+        weixin = ua.match(/MicroMessenger/);
+        if (browser.webkit = !!webkit) browser.version = webkit[1]
+        if (android) os.android = true,
+        os.version = android[2]
+        if (iphone && !ipod) os.ios = os.iphone = true,
+        os.version = iphone[2].replace(/_/g, '.');
+        if (ipad) os.ios = os.ipad = true,
+        os.version = ipad[2].replace(/_/g, '.');
+        if (ipod) os.ios = os.ipod = true,
+        os.version = ipod[3] ? ipod[3].replace(/_/g, '.')  : null
+        if (wp) os.wp = true,
+        os.version = wp[1]
+        if (webos) os.webos = true,
+        os.version = webos[2]
+        if (touchpad) os.touchpad = true
+        if (blackberry) os.blackberry = true,
+        os.version = blackberry[2]
+        if (bb10) os.bb10 = true,
+        os.version = bb10[2]
+        if (rimtabletos) os.rimtabletos = true,
+        os.version = rimtabletos[2]
+        if (playbook) browser.playbook = true
+        if (kindle) os.kindle = true,
+        os.version = kindle[1]
+        if (silk) browser.silk = true,
+        browser.version = silk[1]
+        if (!silk && os.android && ua.match(/Kindle Fire/)) browser.silk = true
+        if (chrome) browser.chrome = true,
+        browser.version = chrome[1]
+        if (firefox) browser.firefox = true,
+        browser.version = firefox[1]
+        if (ie) browser.ie = true,
+        browser.version = ie[1]
+        if (safari && (osx || os.ios)) {
+            browser.safari = true;
+            if (osx) browser.version = safari[1]
+        }
+        if (weixin) browser.weixin = true
+        if (webview) browser.webview = true
+        os.tablet = !!(ipad || playbook || (android && !ua.match(/Mobile/)) || (firefox && ua.match(/Tablet/)) || (ie && !ua.match(/Phone/) && ua.match(/Touch/)));
+        os.phone = !!(!os.tablet && !os.ipod && (android || iphone || webos || blackberry || bb10 || (chrome && ua.match(/Android/)) || (chrome && ua.match(/CriOS\/([\d.]+)/)) || (firefox && ua.match(/Mobile/)) || (ie && ua.match(/Touch/))))
+    }
+    detect.call($, navigator.userAgent);
+    $.__detect = detect
+}) (jQuery);
